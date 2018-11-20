79feb78cf63953f97b4ceced3bd8395c6182bbb6
YilongFreeman <leeddeng@hotmail.com>
2018-11-20 16:12:34 +1300

    Shopbank Finished

--- Shopbank/Shopbank/Controllers/ShopItemController.cs
+++ Shopbank/Shopbank/Controllers/ShopItemController.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Shopbank.Helpers;
 using Shopbank.Models;
 
 namespace Shopbank.Controllers
@@ -14,20 +19,22 @@ namespace Shopbank.Controllers
     public class ShopItemController : ControllerBase
     {
         private readonly ShopbankContext _context;
+        private IConfiguration _configuration;
 
-        public ShopItemController(ShopbankContext context)
+        public ShopItemController(ShopbankContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
-        // GET: api/ShopItem
+        // GET: api/Shop
         [HttpGet]
         public IEnumerable<ShopItem> GetShopItem()
         {
             return _context.ShopItem;
         }
 
-        // GET: api/ShopItem/5
+        // GET: api/Shop/5
         [HttpGet("{id}")]
         public async Task<IActionResult> GetShopItem([FromRoute] int id)
         {
@@ -46,7 +53,7 @@ namespace Shopbank.Controllers
             return Ok(shopItem);
         }
 
-        // PUT: api/ShopItem/5
+        // PUT: api/Meme/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutShopItem([FromRoute] int id, [FromBody] ShopItem shopItem)
         {
@@ -81,7 +88,7 @@ namespace Shopbank.Controllers
             return NoContent();
         }
 
-        // POST: api/ShopItem
+        // POST: api/Meme
         [HttpPost]
         public async Task<IActionResult> PostShopItem([FromBody] ShopItem shopItem)
         {
@@ -96,7 +103,7 @@ namespace Shopbank.Controllers
             return CreatedAtAction("GetShopItem", new { id = shopItem.Id }, shopItem);
         }
 
-        // DELETE: api/ShopItem/5
+        // DELETE: api/Meme/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteShopItem([FromRoute] int id)
         {
@@ -117,9 +124,9 @@ namespace Shopbank.Controllers
             return Ok(shopItem);
         }
 
-        private bool ShopItemExists(int Id)
+        private bool ShopItemExists(int id)
         {
-            return _context.ShopItem.Any(e => e.Id == Id);
+            return _context.ShopItem.Any(e => e.Id == id);
         }
         // GET: api/Meme/Tags
         [Route("tags")]
@@ -132,6 +139,106 @@ namespace Shopbank.Controllers
             var returned = await shops.ToListAsync();
 
             return returned;
+        }
+        [HttpPost, Route("upload")]
+        public async Task<IActionResult> UploadFile([FromForm]ShopImageItem shop)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+            try
+            {
+                using (var stream = shop.Image.OpenReadStream())
+                {
+                    var cloudBlock = await UploadToBlob(shop.Image.FileName, null, stream);
+                    //// Retrieve the filename of the file you have uploaded
+                    //var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+                    if (string.IsNullOrEmpty(cloudBlock.StorageUri.ToString()))
+                    {
+                        return BadRequest("An error has occured while uploading your file. Please try again.");
+                    }
+
+                    ShopItem shopItem = new ShopItem();
+                    shopItem.Title = shop.Title;
+                    shopItem.Tags = shop.Tags;
+
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(stream);
+                    shopItem.Height = image.Height.ToString();
+                    shopItem.Width = image.Width.ToString();
+                    shopItem.Url = cloudBlock.SnapshotQualifiedUri.AbsoluteUri;
+                    shopItem.Uploaded = DateTime.Now.ToString();
+
+                    _context.ShopItem.Add(shopItem);
+                    await _context.SaveChangesAsync();
+
+                    return Ok($"File: {shop.Title} has successfully uploaded");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+
+        }
+
+        private async Task<CloudBlockBlob> UploadToBlob(string filename, byte[] imageBuffer = null, System.IO.Stream stream = null)
+        {
+
+            var accountName = _configuration["AzureBlob:name"];
+            var accountKey = _configuration["AzureBlob:key"]; ;
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imagesContainer = blobClient.GetContainerReference("images");
+
+            string storageConnectionString = _configuration["AzureBlob:connectionString"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Generate a new filename for every new blob
+                    var fileName = Guid.NewGuid().ToString();
+                    fileName += GetFileExtention(filename);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = imagesContainer.GetBlockBlobReference(fileName);
+
+                    if (stream != null)
+                    {
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    }
+                    else
+                    {
+                        return new CloudBlockBlob(new Uri(""));
+                    }
+
+                    return cloudBlockBlob;
+                }
+                catch (StorageException ex)
+                {
+                    return new CloudBlockBlob(new Uri(""));
+                }
+            }
+            else
+            {
+                return new CloudBlockBlob(new Uri(""));
+            }
+
+        }
+
+        private string GetFileExtention(string fileName)
+        {
+            if (!fileName.Contains("."))
+                return ""; //no extension
+            else
+            {
+                var extentionList = fileName.Split('.');
+                return "." + extentionList.Last(); //assumes last item is the extension 
+            }
         }
     }
 }
